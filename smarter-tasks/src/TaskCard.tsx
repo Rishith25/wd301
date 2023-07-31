47d484f4fef95fd8a3b3c31026d4f75c5a4649df
rishith <kamishetty.rishith@gmail.com>
2023-07-31 23:13:07 +0530

    Added TaskCard.tsx file

---
+++ smarter-tasks/src/TaskCard.tsx
@@ -0,0 +1,31 @@
+import "./TaskCard.css";
+
+interface TaskCardInterface {
+  title: string;
+  dueDate?: string;
+  completedAtDate?: string;
+  assigneeName: string;
+}
+
+const TaskCard = ({
+  title,
+  dueDate,
+  completedAtDate,
+  assigneeName,
+}: TaskCardInterface) => {
+  let date;
+  if (dueDate) {
+    date = `Due on: ${dueDate}`;
+  } else {
+    date = `Completed on: ${completedAtDate}`;
+  }
+  return (
+    <div className="TaskItem">
+      <h2 className="text-xl font-bold">{title}</h2>
+      <p>{date}</p>
+      <p>Assignee: {assigneeName}</p>
+    </div>
+  );
+};
+
+export default TaskCard;

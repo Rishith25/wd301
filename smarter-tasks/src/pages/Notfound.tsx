bce355c22e01e7b51e89c30cba2bcb78991a542f
rishith <kamishetty.rishith@gmail.com>
2023-08-03 22:55:03 +0530

    Added Button id

--- smarter-tasks/src/pages/Notfound.tsx
+++ smarter-tasks/src/pages/Notfound.tsx
@@ -6,7 +6,7 @@ const NotFoundPage: React.FC = () => {
     <div className="flex flex-col justify-center items-center h-screen">
       <h1 className="text-4xl font-bold mb-4">404</h1>
       <p className="text-lg text-gray-600 py-3">Page Not Found</p>
-      <button id='HomePage'><a href="/home" className="border border-blue-600 rounded p-1 flex text-lg bg-blue-700 text-white">Back to Home</a></button>
+      <button id='backToHomeButton'><a href="/home" className="border border-blue-600 rounded p-1 flex text-lg bg-blue-700 text-white">Back to Home</a></button>
     </div>
   );
 };

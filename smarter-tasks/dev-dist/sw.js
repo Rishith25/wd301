c3d0374dad3c20f4b669bcdc57150bab087a4cac
rishith <kamishetty.rishith@gmail.com>
2023-08-30 07:41:36 +0530

    build added again

--- smarter-tasks/dev-dist/sw.js
+++ smarter-tasks/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rsmr04g48to"
+    "revision": "0.qb7li7eeipg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- smarter-tasks/src/index.tsx
+++ smarter-tasks/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.js'
 import './index.css'
-import reportWebVitals from "./reportWebVitals.js";
+// import reportWebVitals from "./reportWebVitals.js";
 import { ThemeProvider } from "./context/theme.js";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -11,4 +11,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   </ThemeProvider>,
 )
 
-reportWebVitals();
+// reportWebVitals();
